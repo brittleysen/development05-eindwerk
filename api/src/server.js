8e5a0eabbf4ac749aad2d6a51006f9bf058d459b
Britt Leysen <britt.leysen@student.ehb.be>
2020-12-15 09:43:13 +0100

    db (connection string db): wijziging connection string

--- api/src/server.js
+++ api/src/server.js
@@ -7,15 +7,15 @@ const port = 5000;
 const pg = require ('knex')({
     client: 'pg',
     version: '9.6',
-    searchPath: ['knex', 'puclic'],
-    connenction: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING:'postgres://example:example@localhost:5432/test'
+    searchPath: ['knex', 'public'],
+    connenction: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING:'postgres://example:example@store:5432/brittleysen'
 });
-
+//postgres://example:example@localhost:5432/test
 const app = express();
 http.Server(app);
 
 app.use(bodyParser.json());
-app.use(
+app.use( 
     bodyParser.urlencoded({
         extended: true
     })
@@ -25,7 +25,22 @@ app.use(
 app.get('/test', (req, res) => {
     res.status(200).send();
 })
+app.get('/', async (req, res) => {
+    const result = await pg
+      .select(['uuid', 'title', 'created_at'])
+      .from('story') //tabelnaam
+    res.json({
+        res: result
+    })
+  }) 
+  
 
+let initialiseTables = async () => {
+    await pg.schema.createTable('analogeData', (table) => {
+        table.string('testTab');
+    })
+  }
 
+  initialiseTables()
 
 module.exports = app;
