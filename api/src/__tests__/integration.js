14bd3cb0ae8d52560db2b77550ecd207b910d852
Britt Leysen <britt.leysen@student.ehb.be>
2020-12-20 10:57:45 +0100

    test(integration test GET /plants): check if DB is online

--- api/src/__tests__/integration.js
+++ api/src/__tests__/integration.js
@@ -1,19 +1,22 @@
 const supertest = require('supertest')
 const http = require('http')
 
-const app = require('../index.js')
+const app = require('../server.js')
 const request = supertest(app)
 
+/**
+ * Check if DB is online
+ * @param path /plants
+ * @returns response 200 OK
+ */
 describe('test /GET plants endpoint', () => {
-    test('check if all fields have content', async () => {
-        const data = await request (app).get('/plants')
+    test('check for response 200, OK', async () => {
         try{
-            expect(200)
-            expect(data).notEquals('')
-
+            await request.get('/plants').expect(200) 
         }
         catch(error){
             console.log(error)
         }
     })
 })
+
